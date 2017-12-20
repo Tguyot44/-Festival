5a01dafe56b0bd89f76f98d336de0cc260c5f44b
tguyot <tguyot@btssio-VirtualBox>
2017-12-20 10:59:06 +0100

    Ajout css

--- vues/Login/vLogin.php
+++ vues/Login/vLogin.php
@@ -1,12 +1,15 @@
 <html>
     <head>
+        <meta charset="utf-8" />
+         <link href="css/cssLogin.css" rel="stylesheet" type="text/css">
         <title>Formulaire d'identification</title>
     </head>
     <body>
-        <form action="cLogin.php" method="post">
-            Votre login : <input type="text" name="login">
+        <h1>Festival</h1>
+        <form id="blocLogin" action="cLogin.php" method="post">
+            <p>Votre login :</p> <input type="text" name="login">
             <br />
-            Votre mot de passe : <input type="password" name="pwd"><br />
+            <p>Votre mot de passe : </p><input type="password" name="pwd"><br />
             <input type="submit" value="Connexion">
         </form>
     </body>
